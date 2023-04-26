313f32774166dbf6b67ebbb2011b195d78ad85be
Араканцев Константин Геннадьевич <k.arakantsev@bftcom.com>
2023-04-26 21:46:04 +0300

    shoing mutation progress

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -26,6 +26,10 @@ const TodoForm = () => {
         savedTodo,
         ...(todos || []),
       ]);
+
+      if (ref.current) {
+        ref.current.value = "";
+      }
     },
   });
 
@@ -53,7 +57,9 @@ const TodoForm = () => {
           <input ref={ref} type="text" className="form-control" />
         </div>
         <div className="col">
-          <button className="btn btn-primary">Add</button>
+          <button className="btn btn-primary" disabled={addTodo.isLoading}>
+            {addTodo.isLoading ? "Adding..." : "Add"}
+          </button>
         </div>
       </form>
     </>
