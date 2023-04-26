895d3a6349c0ebb49af79ddd383fdfe24db775d9
Араканцев Константин Геннадьевич <k.arakantsev@bftcom.com>
2023-04-26 18:12:57 +0300

    mutating data

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import "./App.css";
 
 import PostList from "./react-query/PostList";
+import TodoForm from "./react-query/TodoForm";
 import TodoList from "./react-query/TodoList";
 
 function App() {
   return (
     <>
       {/* <TodoList /> */}
-      <PostList />
+      {/* <PostList /> */}
+      <TodoForm />
+      <TodoList />
     </>
   );
 }
--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -1,10 +1,50 @@
-import { useRef } from 'react';
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useRef } from "react";
+import axios from "axios";
+
+import { Todo } from "./hooks/useTodos";
 
 const TodoForm = () => {
   const ref = useRef<HTMLInputElement>(null);
 
+  const queryClient = useQueryClient();
+
+  const addTodo = useMutation({
+    mutationFn: (todo: Todo) => {
+      return axios
+        .post<Todo>("https://jsonplaceholder.typicode.com/todos", todo)
+        .then((res) => res.data);
+    },
+    onSuccess: (savedTodo, newTodo) => {
+      // APPROACH 1: Invalidating the cache (doesn't work with jsonplaceholder)
+      // queryClient.invalidateQueries({
+      //   queryKey: ["todos"],
+      // });
+
+      // APPROACH 2: Updating data in the cache directly
+      queryClient.setQueriesData<Todo[]>(["todos"], (todos) => [
+        savedTodo,
+        ...(todos || []),
+      ]);
+    },
+  });
+
   return (
-    <form className="row mb-3">
+    <form
+      className="row mb-3"
+      onSubmit={(event) => {
+        event.preventDefault();
+
+        if (ref.current?.value) {
+          addTodo.mutate({
+            id: 0,
+            title: ref.current?.value,
+            completed: false,
+            userId: 1,
+          });
+        }
+      }}
+    >
       <div className="col">
         <input ref={ref} type="text" className="form-control" />
       </div>
